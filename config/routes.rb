1f02f323d0c1b7ced150771e412efd99d1781fe6
Joshua Bowers <joshua.bowers+code@gmail.com>
2019-02-08 16:28:00 -0800

    Replacing 'dashboard' in favor of 'front_end', to make nomenclature more descriptive

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'dashboard#index'
+  root to: 'front_end#index'
 
-  match '*path', to: 'dashboard#index', via: :all
+  match '*path', to: 'front_end#index', via: :all
 end
